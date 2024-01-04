346c7a5874a45087a7237e071c7b61ad996903aa
Alex Matiash <matiash.jr@gmail.com>
2024-01-04 10:53:01 +0200

    edit admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import HashTag, Post, Like, Comment
+from user.admin import CoreModelAdmin
+
+admin.site.register(HashTag)
+admin.site.register(Like)
+
+
+@admin.register(Post)
+class PostAdmin(CoreModelAdmin):
+    list_display = ("title", "created_by", "created_at", "updated_at")
+    search_fields = (
+        "title",
+        "created_by__email",
+    )
+    ordering = (
+        "created_by",
+        "-updated_at",
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(CoreModelAdmin):
+    list_display = (
+        "post",
+        "content",
+        "created_by",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("created_by__email",)
+    ordering = (
+        "post",
+        "-updated_at",
+    )
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.admin import CoreModelAdmin
+from user_profile.models import UserProfile, UserProfileFollow
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(CoreModelAdmin):
+    list_display = ("created_by", "bio", "created_at", "updated_at")
+    search_fields = ("created_by__email",)
+    ordering = ("created_by", "-updated_at",)
+
+
+@admin.register(UserProfileFollow)
+class UserProfileFollowAdmin(CoreModelAdmin):
+    list_display = ("created_by", "following", "created_at", "updated_at")
+    search_fields = ("created_by__email",)
+    ordering = ("created_by", "-updated_at",)
