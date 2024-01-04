b7e1c1827600191666654ffa2801327c20ee46d9
Alex Matiash <matiash.jr@gmail.com>
2024-01-04 10:54:02 +0200

    implement models for posts, user and user-profile

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,69 @@
+import os
+import uuid
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from user.models import CoreModel
+from user_profile.models import UserProfile
+
+
+def get_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    dirname = f"{slugify(type(instance).__name__)}s"
+
+    return os.path.join("uploads", dirname, filename)
+
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
+
+class Post(CoreModel):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    hashtags = models.ManyToManyField(
+        HashTag, related_name="posts", blank=True
+    )
+    content = models.TextField()
+    image = models.ImageField(
+        null=True, blank=True, upload_to=get_image_file_path
+    )
+    profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    is_visible = models.BooleanField(default=True)
+    scheduled_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
+
+
+class Like(CoreModel):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="likes"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["post_id", "created_by_id"], name="unique_likes"
+            )
+        ]
+
+    def __str__(self):
+        return f"Liked by {self.created_by}"
+
+
+class Comment(CoreModel):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.id}: {self.content} (by {self.created_by})"
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,66 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.text import slugify
+
+from user.models import CoreModel
+
+
+def get_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.created_by)}-{uuid.uuid4()}{extension}"
+    dirname = f"{slugify(type(instance).__name__)}s"
+
+    return os.path.join("uploads", dirname, filename)
+
+
+class UserProfile(CoreModel):
+    bio = models.TextField()
+    image = models.ImageField(
+        blank=True, null=True, upload_to=get_image_file_path
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["created_by"], name="unique_created_by"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.id}: {self.created_by}"
+
+
+class UserProfileFollow(CoreModel):
+    following = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="followers",
+    )
+    created_by = models.ForeignKey(
+        to=get_user_model(),
+        editable=False,
+        on_delete=models.CASCADE,
+        related_name="followings",
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["created_by", "following"], name="unique_follows"
+            )
+        ]
+
+    def validate_following(self):
+        if self.created_by == self.following:
+            raise ValidationError("Cannot follow/unfollow your own profile.")
+
+    def save(self, *args, **kwargs):
+        self.validate_following()
+        super(UserProfileFollow, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.created_by} follows {self.following}"
